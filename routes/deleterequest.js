cd9b8c96850d457ee5945b4c41fabef71439e5cc
tslime <taha.slime@gmail.com>
2025-10-27 15:59:58 -0400

    Phase 2 almost done

--- routes/deleterequest.js
+++ routes/deleterequest.js
@@ -1,6 +1,30 @@
 module.exports = delete_request;
+const path = __dirname + '/../data/db_test.json';
+const f = require('fs');
 
 function delete_request(req,res){
-    console.log("delete received\n");
-    res.send("delete served\n");
+    let text = f.readFileSync(path,'utf8');
+    let data = JSON.parse(text);
+
+    let i = 0;
+    let b = false;
+    while(i < data.legnth && !b){
+        if(data[i].id === req.body.id){
+            data[i].name = '';
+            b = true;
+        }
+
+        i++;
+    }
+
+
+    if(!b){
+        return res.status(404).json({message:"item not found"});
+    }
+    
+    f.writeFileSync(path,JSON.stringify(data,null,2));
+    text = f.readFileSync(path,'utf8');
+    data = JSON.parse(text);
+
+    res.json(data);
 }
--- routes/putrequest.js
+++ routes/putrequest.js
@@ -1,6 +1,27 @@
 module.exports = put_request;
+const path = __dirname + '/../data/db_test.json';
+const f = require('fs');
 
 function put_request(req,res){
-    console.log("put received");
-    res.send("put served");
+    let text = f.readFileSync(path,'utf8');
+    let data = JSON.parse(text);
+
+    let i = 0;
+    let b = false
+    while(i < data.length && !b){
+        if(data[i].id === req.body.id){
+            data[i].item = req.body.item;
+            b = true;
+        }
+        i++;
+    }
+
+    if(!b){
+        return res.status(404).json({message:"item not found"});
+    }
+    
+    f.writeFileSync(path,JSON.stringify(data,null,2));
+    text = f.readFileSync(path,'utf8');
+    data = JSON.parse(text);
+    res.json(data);
 }
