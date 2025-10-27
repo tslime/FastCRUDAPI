96144bd989b8ea8ac84d813e1c670a9bbe51f85d
tslime <taha.slime@gmail.com>
2025-10-27 11:37:31 -0400

    basic server completed

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const app = express();
 
+
+app.get("/",handle_crud_request);
+
+
+
+
+function handle_crud_request(req,res){
+    console.log("received\n");
+
+    res.send("served\n");
+}
