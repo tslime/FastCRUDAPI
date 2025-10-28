4195d970e2f638606ae5495d2c7a88ad435f5052
tslime <taha.slime@gmail.com>
2025-10-28 14:43:47 -0400

    Security hardened

--- server_mgdb.js
+++ server_mgdb.js
@@ -1,11 +1,27 @@
+//Security
+const cors = require('cors');
+const helmet = require('helmet');
+const rlimit = require('express-rate-limit');
+const window = rlimit({
+  windowMS: 15*60*1000,
+  max: 100,
+});
+
+//Dependencies
 const express = require('express');
 const func_requests = require('./routesmgdb/requests.js');
-
-
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(window);
+
+
+//requests server
 app.use("/",func_requests);
+
+//centralized error handlelr
 app.use((err,req,res,next) => {
   console.error("error: ",err.stack);
   res.status(500).json({error: "Internal server error",message: err.message});
